d69ecadabe807de4ab893d8e2ab2bcf992120b23
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2024-04-23 21:00:05 +0100

    JS updates
    Modified JS code in an effort to make the AI more responsive.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ function showAnswer(response) {
 }
 function generatePoem(event) {
     event.preventDefault();
+    let instructions = document.querySelector("#userinstructions");
     let apiKey = "491ft9f19a10791b7da8b060497d3o64";
-    let context = "you are a polite literate AI that can compose poems.Generate a 4 line poem in basic HTML and separate each line with a <br/>";
-    let prompt = "make a poem based on the input given to you";
+    let context = `you are a polite literate AI that can compose poems.Generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to use the input from ${instructions}.`;
+    let prompt = `make a poem based on ${instructions} given to you.`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(showAnswer);
 }
