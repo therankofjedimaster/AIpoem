40cbd65a7d5f3d2b41e5af84dd264a1b3408f8c6
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2024-04-23 18:23:54 +0100

    Update index.js
    Fixed the js so now it works as intended.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import axios from "axios";
 function showAnswer(response) {
     let poem = document.querySelector("#poem");
-    poemContainer.textContent = response.data.answer;
+    poem.innerHTML = response.data.answer;
 }
 function generatePoem(event) {
     event.preventDefault();
     let apiKey = "491ft9f19a10791b7da8b060497d3o64";
     let context = "you are a polite literate AI that can compose poems.Generate a 4 line poem in basic HTML and separate each line with a <br/>";
-    let prompt = "make a poem based on the topic given to you";
-    let apiUrl =
-        "https://api.shecodes.io/ai/v1/generate?prompt=make a poem based on the topic given to you&context=you are a polite literate AI that can compose poems.Generate a 4 line poem in basic HTML and separate each line with a <br/>&key=491ft9f19a10791b7da8b060497d3o64";
-    poem.innerHTML = axios.get(apiUrl).then(showAnswer);
+    let prompt = "make a poem based on the input given to you";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    axios.get(apiUrl).then(showAnswer);
 }
 
 let poemFormElement = document.querySelector("#poemForm");
